16733f63eeb2d54a25f47bd6f95e3200a1eac37a
Anilabh Barua <anilabh.barua@gmail.com>
2025-05-15 11:23:43 +0530

    #987

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,7 +5,7 @@ import Dashboard from '../pages/Dashboard';
 import Courses from '../pages/Courses';
 import CourseContent from '../pages/CourseContent';
 import Profile from '../pages/Profile';
-import Settings from '../pages/Settings';
+// import Settings from '../pages/Settings';
 import NotFound from '../components/ui/NotFound';
 
 const AppRoutes: React.FC = () => {
@@ -17,7 +17,7 @@ const AppRoutes: React.FC = () => {
           <Route path="courses" element={<Courses />} />
           <Route path="courses/:courseId" element={<CourseContent />} />
           <Route path="profile" element={<Profile />} />
-          <Route path="settings" element={<Settings />} />
+          {/* <Route path="settings" element={<Settings />} /> */}
           <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
